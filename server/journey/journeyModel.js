99f4c9c9cf4b81623058925b408fd0ea39ed3e88
Sara Gee <sgee03@gmail.com>
2016-04-08 11:47:17 -0700

    (feat) Require Crypto, alter Schema (end pnt, start pnt, and hash), create hashing func (#46)
    * Adjust schema to add end,start, and hash
    
    * Inject Crypto
    
    * Require crypto. Create presave function

--- server/journey/journeyModel.js
+++ server/journey/journeyModel.js
@@ -1,10 +1,32 @@
 var mongoose = require('mongoose');
+var crypto = require('crypto');
+var Q = require('q');
 
 var JourneySchema = new mongoose.Schema({
-  journey: {
+  startPoint: {
     type: String,
     required: true
+  },
+  endPoint: {
+    type: String,
+    required: true
+  },
+  hash: {
+    type: String
   }
+});
+
+var createSha = function (points) {
+  var shasum = crypto.createHash('sha1');
+  shasum.update(points);
+  return shasum.digest('hex').slice(0, 5);
+};
+
+JourneySchema.pre('save', function(next) {
+  var journey = this;
+  var hash = createSha(journey.startPoint + journey.endPoint);
+  journey.hash = hash;
+  next();
 });
 
 module.exports = mongoose.model('Journey', JourneySchema);
