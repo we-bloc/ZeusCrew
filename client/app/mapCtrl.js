9c4290d0925b68835f7717df3edd6bd8782e07f7
fswiecki <frances.swiecki@gmail.com>
2016-04-08 14:40:37 -0400

    Add waypoint calculation to the map services

--- client/app/mapCtrl.js
+++ client/app/mapCtrl.js
@@ -1,9 +1,9 @@
 angular.module('roadtrippin.maps', ['gservice'])
   .controller('mapController', function($scope, mapFactory, gservice) {
     $scope.route = {};
-
+    $scope.route.numStops = 2;
     //this is a call to our Google maps API factory for directions
     $scope.getRoute = function() {
-      gservice.calcRoute($scope.route.start, $scope.route.end);
+      gservice.calcRoute($scope.route.start, $scope.route.end, $scope.route.numStops);
     };
   });
