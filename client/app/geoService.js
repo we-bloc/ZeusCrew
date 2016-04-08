a7745d83172321fc128ff9563083426779588dcc
fswiecki <frances.swiecki@gmail.com>
2016-04-08 08:53:27 -0400

    Change style to reflect preferred no spaces

--- client/app/geoService.js
+++ client/app/geoService.js
@@ -1,45 +1,45 @@
 angular.module('gservice', [])
-    .factory('gservice', function($http) {
+    .factory('gservice', function($http){
 
       var googleMapService = {};
 
       // Functions
       // --------------------------------------------------------------
 
-      // New refresh!
-      googleMapService.refresh = function() {
-        initialize();
-      };
-
-      // Initializes the map
-      var map;
-      var directionsDisplay;
+      // Initialize the map
+      var map, directionsDisplay;
       var directionsService = new google.maps.DirectionsService();
 
-      function initialize() {
+      var initialize = function(){
         directionsDisplay = new google.maps.DirectionsRenderer();
         var SF = new google.maps.LatLng(37.7749, -122.4194);
         var mapOptions = {
           zoom: 7,
           center: SF
         };
-        map = new google.maps.Map(document.getElementById("map"), mapOptions);
+        map = new google.maps.Map(document.getElementById('map'), mapOptions);
         directionsDisplay.setMap(map);
-      }
+      };
+
+      // Refresh, to re-initialize the map.
+      // New data could be passed to initialize here
+      googleMapService.refresh = function(){
+        initialize();
+      };
 
       // Refresh the page upon window load. Use the initial latitude and longitude
       google.maps.event.addDomListener(window, 'load',
         googleMapService.refresh());
 
       //calculate a route
-      googleMapService.calcRoute = function(start, end) {
+      googleMapService.calcRoute = function(start, end){
         var request = {
           origin: start,
           destination: end,
           travelMode: google.maps.TravelMode.DRIVING
         };
-        directionsService.route(request, function(result, status) {
-          if (status == google.maps.DirectionsStatus.OK) {
+        directionsService.route(request, function(result, status){
+          if (status == google.maps.DirectionsStatus.OK){
             directionsDisplay.setDirections(result);
           }
         });
