caf1a1fedfa8cba4e438480a7c8ea6cb8343e095
fswiecki <frances.swiecki@gmail.com>
2016-04-08 14:40:37 -0400

    Add waypoint calculation to the map services

--- client/app/geoService.js
+++ client/app/geoService.js
@@ -21,18 +21,39 @@ angular.module('gservice', [])
         directionsDisplay.setMap(map);
       };
 
+      //get waypoints
+      var getWaypoints = function (waypointArray, numStops) {
+        var points = [];
+        var stopDistance = Math.floor(waypointArray.length / (numStops + 1));
+        for (i = 0; i < numStops; i++) {
+          points.push(stopDistance + (stopDistance * i));
+        }
+
+        //initialize an empty array to hold our waypoints  
+        var waypoints = [];
+        points.forEach(function (index) {
+          //each waypoint will be an object with a lat and lng
+          var waypoint = {
+            lat: waypointArray[index].lat(),
+            lng: waypointArray[index].lng()
+          };
+          waypoints.push(waypoint);
+        });
+        console.log(waypoints);
+      };
+
       // Refresh, to re-initialize the map.
       // New data could be passed to initialize here
       googleMapService.refresh = function () {
         initialize();
       };
 
-      // Refresh the page upon window load. Use the initial latitude and longitude
+      // Refresh the page upon window load.
       google.maps.event.addDomListener(window, 'load',
         googleMapService.refresh());
 
       //calculate a route
-      googleMapService.calcRoute = function (start, end) {
+      googleMapService.calcRoute = function (start, end, numStops) {
         var request = {
           origin: start,
           destination: end,
@@ -40,10 +61,12 @@ angular.module('gservice', [])
         };
         directionsService.route (request, function(result, status) {
           if (status == google.maps.DirectionsStatus.OK) {
+            getWaypoints(result.routes[0].overview_path, numStops);
             directionsDisplay.setDirections(result);
           }
         });
       };
+
 
       return googleMapService;
     });
