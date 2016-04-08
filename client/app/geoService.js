f1ff32147af05e51be532bd1ee2197575014ae81
Frances Swiecki-Bernhardt <frances.swiecki@gmail.com>
2016-04-08 15:25:06 -0400

    (Feat) Add waypoint calculation (#48)
    * Add waypoint calculation to the map services
    
    * Add waypoint calculation to the map services
    
    * Add waypoint calculation to the map services
    
    * Waypoints now appear on the map!
    
    * Add waypoint calculation to the map services
    
    * Waypoints now appear on the map!
    
    * Solve trailing whitespace issues
    
    * Add waypoint calculation to the map services
    
    * Waypoints now appear on the map!
    
    * Rebase (again)

--- client/app/geoService.js
+++ client/app/geoService.js
@@ -21,18 +21,39 @@ angular.module('gservice', [])
         directionsDisplay.setMap(map);
       };
 
+      //get waypoints
+      var getWaypoints = function (waypointArray, numStops) {
+        var points = [];
+        var stopDistance = Math.floor(waypointArray.length / (numStops + 1));
+        for (i = 0; i < numStops; i++) {
+          points.push(stopDistance + (stopDistance * i));
+        }
+
+        //initialize an empty array to hold our waypoints
+        var waypoints = [];
+        points.forEach(function (index) {
+          //each waypoint will be an object with a lat and lng
+          var waypoint = {
+            lat: waypointArray[index].lat(),
+            lng: waypointArray[index].lng()
+          };
+          waypoints.push(waypoint);
+        });
+        return waypoints;
+      };
+
       // Refresh, to re-initialize the map.
       // New data could be passed to initialize here
       googleMapService.refresh = function () {
         initialize();
       };
 
-      // Refresh the page upon window load. Use the initial latitude and longitude
+      // Refresh the page upon window load.
       google.maps.event.addDomListener(window, 'load',
         googleMapService.refresh());
 
       //calculate a route
-      googleMapService.calcRoute = function (start, end) {
+      googleMapService.calcRoute = function (start, end, numStops) {
         var request = {
           origin: start,
           destination: end,
@@ -40,10 +61,31 @@ angular.module('gservice', [])
         };
         directionsService.route (request, function(result, status) {
           if (status == google.maps.DirectionsStatus.OK) {
-            directionsDisplay.setDirections(result);
+            var stops = [];
+            var waypoints = getWaypoints(result.routes[0].overview_path, numStops);
+            waypoints.forEach(function (w) {
+              stops.push({
+                location: new google.maps.LatLng(w.lat, w.lng),
+                stopover: true
+              });
+            });
+            var wyptRequest = {
+              origin: start,
+              destination: end,
+              waypoints: stops,
+              optimizeWaypoints: true,
+              travelMode: google.maps.TravelMode.DRIVING
+            };
+            directionsService.route(wyptRequest, function(response, status) {
+              if (status === google.maps.DirectionsStatus.OK) {
+                directionsDisplay.setDirections(response);
+                var route = response.routes[0];
+              }
+            });
           }
         });
       };
+
 
       return googleMapService;
     });
