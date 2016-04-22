e920703fb551e26b0bc345576eb4e25a452fbe49
Mark Keith <mark.r.keith@gmail.com>
2016-04-22 16:24:41 -0700

    (feat) Set up basic html and js for the friendRequest autocomplete

--- client/app/dashboard/dashboardCtrl.js
+++ client/app/dashboard/dashboardCtrl.js
@@ -1,3 +1,22 @@
-angular.module('dashboard', [])
-  .controller('dashboardController', [function(){
-  }])
+angular.module('dashboard', ['ngMaterial'])
+  .controller('dashboardController', dashboardController);
+
+function dashboardController () {
+  var self = this;
+  // This populates with all non-friended users
+  // self.users = getUsers();
+  self.users = [{name:'Mark'}, {name:'Stuart'}, {name:'Phil'}, {name:'Dan'}, {name:'Nassir'}, {name:'Trevor'}]
+  self.selectedUser = null;
+  self.searchText = null;
+  self.querySearch = querySearch;
+
+  // search the current searchText for a matching list of users
+  function querySearch (query) {};
+
+  // http request to server... will return all non-friended users
+  function getUsers () {};
+
+  // creates a filter function for a query string
+  function createFilterFor (query) {};
+};
+
