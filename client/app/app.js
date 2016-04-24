52bb3d9edd51f1840021efb955c591e0348e16cf
Mark Keith <mark.r.keith@gmail.com>
2016-04-24 10:33:54 -0700

    (feat) Add modal for friendRequests on the navbar

--- client/app/app.js
+++ client/app/app.js
@@ -60,7 +60,7 @@ angular.module('roadtrippin', [
 
   $httpProvider.interceptors.push('AttachTokens');
 })
-.controller('mainController', ['$scope','mapFactory', 'dashboardFactory', function($scope,mapFactory,dashboardFactory){
+.controller('mainController', ['$scope','mapFactory', 'dashboardFactory', '$mdDialog', '$mdMedia', function($scope,mapFactory,dashboardFactory,$mdDialog, $mdMedia){
   $scope.notifications;
   $scope.signout = function () {
     $scope.notifications = [];
@@ -73,6 +73,40 @@ angular.module('roadtrippin', [
     });
   };
   $scope.getNotifications();
+
+  function DialogController($scope, $mdDialog, notifs) {
+    $scope.notifs = notifs;
+    $scope.closeDialog = function() {
+      $mdDialog.hide();
+    }
+  };
+
+  $scope.showNotifications = function($event) {
+    var parentEl = angular.element(document.body);
+    $mdDialog.show({
+      parent: parentEl,
+      targetEvent: $event,
+      template: 
+          '<md-dialog aria-label="List dialog">' +
+           '  <md-dialog-content>'+
+           '    <md-list>'+
+           '      <md-list-item ng-repeat="item in notifs track by $index">'+
+           '       <p>Number {{item}}</p>' +
+           '      '+
+           '    </md-list-item></md-list>'+
+           '  </md-dialog-content>' +
+           '  <md-dialog-actions>' +
+           '    <md-button ng-click="closeDialog()" class="md-primary">' +
+           '      Close Dialog' +
+           '    </md-button>' +
+           '  </md-dialog-actions>' +
+           '</md-dialog>',
+      locals: {
+        notifs: $scope.notifications
+      },
+      controller: DialogController
+    })
+  }
 }])
 
 .factory('AttachTokens', function ($window) {
