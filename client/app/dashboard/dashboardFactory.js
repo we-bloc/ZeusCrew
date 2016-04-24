6dfeada61ba6536c4b0373c56ad7bab54ab7cd98
Mark Keith <mark.r.keith@gmail.com>
2016-04-24 13:34:48 -0700

    (fix) Fix such weird conflicts

--- client/app/dashboard/dashboardFactory.js
+++ client/app/dashboard/dashboardFactory.js
@@ -19,10 +19,9 @@ angular.module('dashboard.factory', [])
         url: '/newRequest',
         data: userObj
       });
-<<<<<<< 3b0ecd4098f44cba0d56f65f2e38676a4f9ee5ff
-  };
-=======
+
     };
+
 
     var reqResponse = function (bool) {
       return $http({
@@ -30,17 +29,13 @@ angular.module('dashboard.factory', [])
         url: '/reqResponse',
         data: bool
       });
-    }
->>>>>>> (refactor) Add the dashboardFactory file
+    };
 
     return {
       getNotifications: getNotifications,
       getUsers: getUsers,
-<<<<<<< 3b0ecd4098f44cba0d56f65f2e38676a4f9ee5ff
-      sendRequest: sendRequest
-=======
       sendRequest: sendRequest,
       reqResponse: reqResponse
->>>>>>> (refactor) Add the dashboardFactory file
+
     };
   });
