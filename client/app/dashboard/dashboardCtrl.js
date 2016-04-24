0e78f5042b45af377e25fd617a3b63a623e78737
Mark Keith <mark.r.keith@gmail.com>
2016-04-23 17:20:23 -0700

    (feat) Sync up to backend

--- client/app/dashboard/dashboardCtrl.js
+++ client/app/dashboard/dashboardCtrl.js
@@ -1,6 +1,6 @@
 angular.module('dashboard', ['ngMaterial'])
   .controller('dashboardController', dashboardCtrl)
-  .factory('dashboardFactory', function($http) {
+  .factory('dashboardFactory', function($http, $q) {
     var getNotifications = function(){
       //HTTP request to get users notifications
       //Promise
@@ -9,87 +9,56 @@ angular.module('dashboard', ['ngMaterial'])
         url: '/myNotifications'
       })
     };
+
+    var getUsers = function() {
+      console.log('Is this running?')
+      return $http({
+        method: 'GET',
+        url: '/myUsers'
+      });
+    };
+    var sendRequest = function (userObj) {
+      $http({
+        method: 'PUT',
+        url: '/newRequest',
+        data: userObj
+      }).then(function(){
+        console.log(userObj);
+      })
+  };
+
     return {
-      getNotifications: getNotifications
+      getNotifications: getNotifications,
+      getUsers: getUsers,
+      sendRequest: sendRequest
     };
   });
 
-function dashboardCtrl ($timeout, $q, $http) {
+function dashboardCtrl ($timeout, $q, $http, dashboardFactory) {
   var self = this;
-  // This populates with all non-friended users
-  self.users = getUsers();
 
-  /*self.users = [
-  {
-    firstName:'Mark',
-    lastName: 'Keith',
-    email: 'mark.r.keith@gmail.com',
-    userName: 'mkeith121'
-  }, 
-  {
-    firstName:'Mark',
-    lastName: 'French',
-    email: 'mark.french@gmail.com',
-    userName: 'mfrench'
-  }, 
-  {
-    firstName:'Phil',
-    lastName: 'Clauss',
-    email: 'phil.clauss@gmail.com',
-    userName: 'philthyclauss'
-  }, 
-  {
-    firstName:'Dan',
-    lastName: 'Savage',
-    email: 'dan.savage@gmail.com',
-    userName: 'danthesavage'
-  }, 
-  {
-    firstName:'Nassir',
-    lastName: 'Zeinu',
-    email: 'sexy.beast@gmail.com',
-    userName: 'servermasterExtraordinaire'
-  }, 
-  {
-    firstName:'Trevor',
-    lastName: 'Pace',
-    email: 'wonderful.man@gmail.com',
-    userName: 'knowsAllTheThings'
-  }]*/
+  self.users; 
+
+  dashboardFactory.getUsers().then(function(users){
+    console.log(users.data);
+    self.users = users.data;
+  });
 
   self.selectedUser = null;
   self.searchText = null;
   self.querySearch = querySearch;
   self.showSuccessAlert = false;
 
-  self.testFunc = function() {
-    console.log(self.selectedUser);
-    self.selectedUser = null;
-    self.searchText = null;
-    self.showSuccess();
-  };
 
   self.showSuccess = function () {
     self.showSuccessAlert = !self.showSuccessAlert;
   };
 
   //These are the functions that send the friend request
-
-  self.sendRequest = sendRequest;
-
-  function sendRequest (userObj) {
-    var deferred = $q.defer();
-      $http({
-        method: 'PUT',
-        url: '/newRequest',
-        data: userObj
-      }).then(function (res) {
-        deferred.resolve (res.data);
-      }).catch(function (err) {
-        deferred.reject (err);
-      });
-      return deferred.promise;
+  self.sendRequest = function() {
+    dashboardFactory.sendRequest(self.selectedUser);
   };
+
 
   // search the current searchText for a matching list of users
   function querySearch (query) {
@@ -100,33 +69,19 @@ function dashboardCtrl ($timeout, $q, $http) {
   };
 
   // http request to server... will return all non-friended users
-  function getUsers () {
-    var deferred = $q.defer();
-      $http({
-        method: 'GET',
-        url: '/myUsers'
-      }).then(function (res) {
-        deferred.resolve (res.data);
-      }).catch(function (err) {
-        deferred.reject (err);
-      });
-      return deferred.promise;
-  };
 
   // creates a filter function for a query string
   function createFilterFor (query) {
     var lowercaseQuery = angular.lowercase(query);
     return function filterFn(user) {
       //Create lowercase versions of all searcheable data
-      var firstName = angular.lowercase(user.firstName);
-      var lastName = angular.lowercase(user.lastName);
-      var email = angular.lowercase(user.email);
-      var userName = angular.lowercase(user.userName);
+      var firstName = angular.lowercase(user.firstname);
+      var lastName = angular.lowercase(user.lastname);
+      var userName = angular.lowercase(user.username);
 
       // return an array of objects that include the query
       return (firstName.includes(lowercaseQuery) || 
               lastName.includes(lowercaseQuery) ||
-              email.includes(lowercaseQuery) ||
               userName.includes(lowercaseQuery));
     };
   };
